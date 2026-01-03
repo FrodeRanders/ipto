e0256ac326c66b5ec64cbb0eac41d93c82705051
Frode Randers <frode.randers@gmail.com>
2026-01-03 13:30:58 +0100

    Added specific operations wiring (for dev), playing with logging

--- repo-cdi/src/main/java/org/gautelis/ipto/bootstrap/IptoBootstrap.java
+++ repo-cdi/src/main/java/org/gautelis/ipto/bootstrap/IptoBootstrap.java
@@ -1,6 +1,7 @@
 package org.gautelis.ipto.bootstrap;
 
 import graphql.GraphQL;
+import graphql.schema.DataFetcher;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Instance;
@@ -8,15 +9,20 @@ import jakarta.inject.Inject;
 import org.gautelis.ipto.config.IptoConfig;
 import org.gautelis.ipto.graphql.configuration.Configurator;
 import org.gautelis.ipto.graphql.configuration.OperationsWireParameters;
+import org.gautelis.ipto.graphql.model.Query;
 import org.gautelis.ipto.repo.model.Repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.quarkus.runtime.Startup;
 
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
+
+import static org.gautelis.ipto.graphql.runtime.RuntimeOperators.MAPPER;
+import static org.gautelis.ipto.graphql.runtime.RuntimeService.headHex;
 
 @Startup
 @ApplicationScoped
@@ -56,12 +62,14 @@ public class IptoBootstrap {
                         sdlResource + " not found on classpath"
                 )
         )) {
-            Consumer<OperationsWireParameters> operationsWireBlock = null;
-            if (!wiring.isUnsatisfied()) {
-                operationsWireBlock = params -> wiring.forEach(w -> w.wire(params));
-            }
+            //Consumer<OperationsWireParameters> operationsWireBlock = null;
+            //if (!wiring.isUnsatisfied()) {
+            //    operationsWireBlock = params -> wiring.forEach(w -> w.wire(params));
+            //}
+            //
+            //Optional<GraphQL> gql = Configurator.load(repository, reader, operationsWireBlock, System.out);
 
-            Optional<GraphQL> gql = Configurator.load(repository, reader, operationsWireBlock, System.out);
+            Optional<GraphQL> gql = Configurator.load(repository, reader, IptoBootstrap::wireOperations, System.out);
             if (gql.isEmpty()) {
                 throw new IllegalStateException("Failed to load GraphQL configuration");
             }
@@ -76,5 +84,133 @@ public class IptoBootstrap {
 
     public GraphQL graphQL() {
         return graphQL;
+    }
+
+    static void wireOperations(OperationsWireParameters params) {
+        // Query::yrkanRaw(id : UnitIdentification!) : Bytes
+        {
+            String type = "Query";
+            String operationName = "yrkanRaw";
+            String parameterName = "id";
+            String outputType = "Bytes";
+
+            DataFetcher<?> rawUnitById = env -> {
+                //**** Executed at runtime **********************************
+                // This closure captures its environment, so at runtime
+                // the wiring preamble will be available.
+                //***********************************************************
+                if (log.isTraceEnabled()) {
+                    log.trace("\u21a9 {}::{}({}) : {}", type, operationName, env.getArguments(), outputType);
+                }
+
+                Query.UnitIdentification id = MAPPER.convertValue(env.getArgument(parameterName), Query.UnitIdentification.class);
+                return params.runtimeService().loadRawUnit(id.tenantId(), id.unitId());
+            };
+
+            params.runtimeWiring().type(type, t -> t.dataFetcher(operationName, rawUnitById));
+            log.info("\u21af Wiring: {}::{}(...) : {}", type, operationName, outputType);
+        }
+
+        // Query::yrkan(id : UnitIdentification!) : Yrkan
+        {
+            String type = "Query";
+            String operationName = "yrkan";
+            String parameterName = "id";
+            String outputType = "Yrkan";
+
+            DataFetcher<?> unitById = env -> {
+                //**** Executed at runtime **********************************
+                // This closure captures its environment, so at runtime
+                // the wiring preamble will be available.
+                //***********************************************************
+                if (log.isTraceEnabled()) {
+                    log.trace("\u21a9 {}::{}({}) : {}", type, operationName, env.getArguments(), outputType);
+                }
+
+                Query.UnitIdentification id = MAPPER.convertValue(env.getArgument(parameterName), Query.UnitIdentification.class);
+                return params.runtimeService().loadUnit(id.tenantId(), id.unitId());
+            };
+
+            params.runtimeWiring().type(type, t -> t.dataFetcher(operationName, unitById));
+            log.info("\u21af Wiring: {}::{}(...) : {}", type, operationName, outputType);
+        }
+
+        // Query::yrkandenRaw(filter: Filter!) : Bytes
+        {
+            String type = "Query";
+            String operationName = "yrkandenRaw";
+            String parameterName = "filter";
+            String outputType = "Bytes";
+
+            DataFetcher<?> rawUnitsByFilter = env -> {
+                //**** Executed at runtime **********************************
+                // This closure captures its environment, so at runtime
+                // the wiring preamble will be available.
+                //***********************************************************
+
+                if (log.isTraceEnabled()) {
+                    log.trace("\u21a9 {}::{}({}) : {}", type, operationName, env.getArguments(), outputType);
+                }
+
+                Query.Filter filter = MAPPER.convertValue(env.getArgument(parameterName), Query.Filter.class);
+
+                return params.runtimeService().searchRaw(filter);
+            };
+
+            params.runtimeWiring().type(type, t -> t.dataFetcher(operationName, rawUnitsByFilter));
+            log.info("\u21af Wiring: {}::{}(...) : {}", type, operationName, outputType);
+        }
+
+        // Query::yrkanden(filter: Filter!) : [Yrkan]
+        {
+            String type = "Query";
+            String operationName = "yrkanden";
+            String parameterName = "filter";
+            String outputType = "[Yrkan]";
+
+            DataFetcher<?> unitsByFilter = env -> {
+                //**** Executed at runtime **********************************
+                // This closure captures its environment, so at runtime
+                // the wiring preamble will be available.
+                //***********************************************************
+                if (log.isTraceEnabled()) {
+                    log.trace("\u21a9 {}::{}({}) : {}", type, operationName, env.getArguments(), outputType);
+                }
+
+                Query.Filter filter = MAPPER.convertValue(env.getArgument(parameterName), Query.Filter.class);
+
+                return params.runtimeService().search(filter);
+            };
+
+            params.runtimeWiring().type(type, t -> t.dataFetcher(operationName, unitsByFilter));
+            log.info("\u21af Wiring: {}::{}(...) : {}", type, operationName, outputType);
+        }
+
+        // Mutations::lagraUnitRaw(data : Bytes!) : Dataleverans
+        {
+            String type = "Mutation";
+            String operationName = "lagraUnitRaw";
+            String parameterName = "data";
+            String outputType = "Dataleverans";
+
+            DataFetcher<?> storeJson = env -> {
+                //**** Executed at runtime **********************************
+                // This closure captures its environment, so at runtime
+                // the wiring preamble will be available.
+                //***********************************************************
+
+                Map<String, Object> args = env.getArguments();
+                byte[] bytes = (byte[]) args.get("data"); // Connection to schema
+
+                if (log.isTraceEnabled()) {
+                    log.trace("\u21a9 {}::{}({}) : {}", type, operationName, headHex(bytes, 16), outputType);
+                }
+
+                return params.runtimeService().storeRawUnit(bytes);
+            };
+
+            params.runtimeWiring().type(type, t -> t.dataFetcher(operationName, storeJson));
+            log.info("\u21af Wiring: {}::{}(...) : {}", type, operationName, outputType);
+        }
     }
 }
