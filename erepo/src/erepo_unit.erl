eecac513eb9240beac4a24debbdc1840b9c40141
Frode Randers <frode.randers@gmail.com>
2026-02-04 13:18:07 +0100

    UUIDv4 -> UUIDv7

--- erepo/src/erepo_unit.erl
+++ erepo/src/erepo_unit.erl
@@ -38,10 +38,13 @@ to_persist_map(#unit{
     end.
 
 make_corrid() ->
-    %% UUIDv4 textual representation. PG expects valid UUID input.
-    <<A:32, B:16, C0:16, D0:16, E:48>> = crypto:strong_rand_bytes(16),
-    C = (C0 band 16#0FFF) bor 16#4000,  %% version 4
-    D = (D0 band 16#3FFF) bor 16#8000,  %% RFC4122 variant
+    %% UUIDv7 textual representation: sortable by unix epoch milliseconds.
+    TsMs0 = erlang:system_time(millisecond),
+    TsMs = TsMs0 band 16#FFFFFFFFFFFF,
+    <<R0:80>> = crypto:strong_rand_bytes(10),
+    RandA = (R0 bsr 68) band 16#FFF,
+    RandB = (R0 bsr 6) band 16#3FFFFFFFFFFFFFFF,
+    <<A:32, B:16, C:16, D:16, E:48>> = <<TsMs:48, 7:4, RandA:12, 2:2, RandB:62>>,
     iolist_to_binary(
       io_lib:format("~8.16.0b-~4.16.0b-~4.16.0b-~4.16.0b-~12.16.0b", [A, B, C, D, E])
     ).
